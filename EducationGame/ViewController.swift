c542474dfc7a525c50268d6754275e895c2788a7
Abha Vedula <abhaved@fb.com>
2016-07-18 17:23:05 -0700

    Initial Commit

---
+++ EducationGame/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  EducationGame
+//
+//  Created by Abha Vedula on 7/18/16.
+//  Copyright © 2016 Abha Vedula. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
